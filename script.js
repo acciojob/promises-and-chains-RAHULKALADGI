15c986f40cf2b627c65d0c0b38656f1de13a8444
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-08-31 21:55:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const name = document.getElementById("name");
+const age = document.getElementById("age");
+const btn = document.getElementById("btn");
+
+function onSubmit(e) {
+	e.preventDefault();
+	if(name.value==="" || age.value==="") {
+		return;
+	}
+	if(age.value > 18) {
+		alert(`Welcome, ${name.innerText}. You can vote.`);
+		return;
+	}
+	else {
+		alert(`Oh sorry ${name.innerText}. you aren't old enough.`);
+	}
+}
+btn.addEventListener("click" , onSubmit);
